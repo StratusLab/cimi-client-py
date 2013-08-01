f7805bfdd2175e1ed446f921847757e7e88c4de5
Charles Loomis <cal@sixsq.com>
2013-08-01 21:19:54 +0200

    skeleton of build infrastructure

---
+++ pypi/src/main/python/setup.py
@@ -0,0 +1,60 @@
+from distutils.core import setup
+
+import os
+import os.path
+
+try:
+    rootdir = os.path.join('lib','stratuslab','python')
+    for file in os.listdir(rootdir):
+        src = os.path.join(rootdir, file)
+        os.symlink(src, file)
+except:
+    # assume links have already been made
+    pass
+
+setup(
+    name='stratuslab-cimi-client',
+    version='${project.version}',
+    author='StratusLab',
+    author_email='contact@stratuslab.eu',
+    url='http://stratuslab.eu/',
+    license='Apache Software License 2.0',
+    description='${project.description}',
+    long_description=open('README.txt').read(),
+
+    scripts=[
+        'bin/cimi',
+        ],
+
+     packages=[
+        'stratuslab',
+        'stratuslab.cimi',
+        ],
+
+    data_files=[
+        ('java', ['java/metadata-${metadata.version}-jar-with-dependencies.jar']),
+        ('share/vm', ['share/vm/schema.one']),
+        ('share/template', ['share/template/manifest.xml.tpl']),
+        ('conf', ['conf/stratuslab-user.cfg.ref']),
+        ('Scripts', ['windows/cimi.bat']),
+        ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: System :: Distributed Computing',
+        ],
+
+    install_requires=[
+        "requests >= 1.2.3",
+    ],
+)
---
+++ pypi/src/main/resources/README.txt
@@ -0,0 +1,28 @@
+======================
+StratusLab CIMI Client
+======================
+
+This project contains a CIMI (Cloud Infrastructure Management 
+Interface) client.  It provides both a command line client for 
+direct use and a library for programmatic access. 
+
+Installation
+============
+
+This package is intended to be installed via pip.  The following
+should 'just work':
+
+    pip install stratuslab-cimi-client
+
+If this is done system-wide, the PYTHONPATH and PATH should already be
+set up correctly.  If this is a local installation, then you may need
+to set those environmental variables by hand. 
+
+See the tutorials on the StratusLab website on how to use the client.
+
+
+Feedback
+========
+
+To provide feedback about the client or to report bugs with the code
+or packaging, please send an email to support@stratuslab.eu.
